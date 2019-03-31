f8a8e5917b667397aef33948ef457a131d89d0db
Erik C. Thauvin <erik@thauvin.net>
2019-03-30 20:26:30 -0700

    Updated wrappers.

--- updatewrappers.sh
+++ updatewrappers.sh
@@ -3,12 +3,14 @@
 echo "semver"
 gradle -q wrapper
 kobaltw --update --log 0
+cp -f /c/kobalt/kobalt/wrapper/kobalt-wrapper.jar /k/java/semver/kobalt/wrapper
 ./kobaltw --version
 cd examples/java
 echo
 echo "examples/java"
 gradle -q wrapper
 kobaltw --update --log 0
+cp -f /c/kobalt/kobalt/wrapper/kobalt-wrapper.jar /k/java/semver/kobalt/wrapper
 ./kobaltw --version
 echo
 echo "examples/kotlin"
