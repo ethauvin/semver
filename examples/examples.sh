9794fab00f319c322151fd2a61884ad5d827f1fb
Erik C. Thauvin <erik@thauvin.net>
2019-04-05 21:59:43 -0700

    Updated shell scripts.

--- examples/examples.sh
+++ examples/examples.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <arg> [...]"
+    echo "Usage: $0 <arg ...>"
     exit 1
 fi
+
+# set the examples directories
+declare -a examples=(
+    "java"
+    "kotlin")
 
 dir=$(dirname "$(readlink -f "$0")")
 cyan=$(tput setaf 6)
 normal=$(tput sgr0)
 
-for ex in "java" "kotlin"; do
+i=0
+for ex in "${examples[@]}"; do
+    if [ $i -ne 0 ]
+    then
+        read -p "Press [Enter] key to continue..."
+        clear
+    fi
     cd "$dir/$ex" || exit 1
-    echo "> Project: ${cyanlor}${ex}${normal}"
+    echo "> Project: ${cyan}${ex}${normal}"
     ./gradlew --console=plain --no-build-cache clean "$@" || exit 1
-    echo
+    (( i++ ))
 done
--- updatewrappers.sh
+++ updatewrappers.sh
@@ -1,30 +1,60 @@
 #!/bin/bash
 
-export JAVA_HOME="$JAVA8_HOME"
-export PATH="$(cygpath "$JAVA_HOME")/bin:$PATH"
+#
+# Version: 1.0
+#
 
-declare -a dirs=("${PWD##*/}" "examples/java" "examples/kotlin")
+# set the examples directories
+declare -a dirs=(
+    "${PWD##*/}"
+    "examples/java"
+    "examples/kotlin")
+java8=true
+
 pwd=$PWD
+cyan=$(tput setaf 6)
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+std=$(tput sgr0)
 
+###
+
+if [ "$java8" = true ]
+then
+    export JAVA_HOME="$JAVA8_HOME"
+    export PATH="$(cygpath "$JAVA_HOME")/bin:$PATH"
+fi
+
+kVer=$(kobaltw --version | awk '{print substr($2, 1, length($2)-1)}')
 updateWrappers() {
+    curVer="$(gradle --version | awk '/Gradle/ {print $2}')"
     if [ -d gradle ]; then
-        gradle -q --console=plain wrapper
-        echo -e "    $(./gradlew --version | grep Gradle)"
+        if [ "$curVer" != "$(./gradlew --version | awk '/Gradle/ {print $2}')" ]; then
+            gradle -q --console=plain wrapper
+            echo -e "        $(./gradlew --version | awk '/Gradle/') ${green}UPDATED${std}"
+        else
+            echo -e "        Gradle $curVer UP-TO-DATE"
+        fi
     fi
     if [ -d kobalt ]; then
-        ./kobaltw --update --log 0
-        echo -e "    Kobalt $(cut -d "=" -f 2 kobalt/wrapper/kobalt-wrapper.properties)"
+        kw=$(cut -d "=" -f 2 kobalt/wrapper/kobalt-wrapper.properties)
+        if [ "$kw" = "$kVer" ]
+        then
+            echo -e "        Kobalt $kw UP-TO-DATE"
+        else
+            echo "kobalt.version=$kVer" > kobalt/wrapper/kobalt-wrapper.properties
+            echo -e "        Kobalt $kVer ${green}UPDATED${std}"
+        fi
     fi
 }
 
-color=$(tput setaf 6)
-normal=$(tput sgr0)
+echo -e "Updating wrappers..."
 
 for d in "${dirs[@]}"; do
     if [ -d "$d" ]; then
         cd "$d" || exit 1
     fi
-    echo -e "${color}${d}${normal}"
+    echo -e "    ${cyan}${d}${std}"
     updateWrappers
     cd "$pwd"
 done
