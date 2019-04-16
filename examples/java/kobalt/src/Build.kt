fbdd6661ead04a684337032f11d9f310204355d1
Erik C. Thauvin <erik@thauvin.net>
2019-04-15 21:24:20 -0700

    Cleanup/

--- examples/java/kobalt/src/Build.kt
+++ examples/java/kobalt/src/Build.kt
@@ -2,10 +2,7 @@ import com.beust.kobalt.*
 import com.beust.kobalt.plugin.application.*
 import com.beust.kobalt.plugin.apt.*
 import com.beust.kobalt.plugin.java.javaCompiler
-import com.beust.kobalt.plugin.publish.*
 import com.beust.kobalt.plugin.packaging.*
-import java.io.FileInputStream
-import java.util.*
 
 // ./kobaltw run
 
@@ -13,36 +10,13 @@ val bs = buildScript {
     repos(localMaven())
 }
 
-fun StringBuilder.prepend(s: String): StringBuilder {
-    if (this.isNotEmpty()) {
-        this.insert(0, s)
-    }
-    return this
-}
-
 val example = project {
 
     name = "example"
-
-    fun versionFor(): String {
-        val propsFile = "version.properties"
-        val majorKey = "version.major"
-        val minorKey = "version.minor"
-        val patchKey = "version.patch"
-        val metaKey = "version.buildmeta"
-        val preKey = "version.prerelease"
-
-        val p = Properties().apply { FileInputStream(propsFile).use { fis -> load(fis) } }
-
-        return (p.getProperty(majorKey, "1") + "." + p.getProperty(minorKey, "0") + "." + p.getProperty(patchKey, "0")
-                + StringBuilder(p.getProperty(preKey, "")).prepend("-")
-                + StringBuilder(p.getProperty(metaKey, "")).prepend("+"))
-    }
-
-    version = versionFor()
+    version = "1.0"
 
     val mainClassName = "com.example.Example"
-    val processorJar = "net.thauvin.erik:semver:1.0.1"
+    val processorJar = "net.thauvin.erik:semver:1.2.0"
 
     dependencies {
         apt(processorJar)
@@ -51,10 +25,6 @@ val example = project {
 
     apt {
         //outputDir = "../src/generated/java/"
-    }
-
-    install {
-        target = "deploy"
     }
 
     javaCompiler {
