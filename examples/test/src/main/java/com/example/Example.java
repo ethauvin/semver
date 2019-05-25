c920adf2c53f11a13984c85f2ec25168518d7478
Erik C. Thauvin <erik@thauvin.net>
2019-05-25 14:30:33 -0700

    Added test example.

---
+++ examples/test/src/main/java/com/example/Example.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import net.thauvin.erik.semver.Version;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+@Version(properties = "example.properties", template = "example.mustache", className = "ExampleVersion",
+    keysPrefix = "example.")
+public final class Example {
+    /**
+     * Command line interface.
+     *
+     * @param args The command line parameters.
+     */
+    public static void main(final String... args) {
+        final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US);
+
+        System.out.println("-- From Example -------------------------------------");
+
+        System.out.println("  " + ExampleVersion.PROJECT + ' ' + ExampleVersion.VERSION
+            + " (" + sdf.format(ExampleVersion.BUILDDATE) + ')');
+
+        System.out.println("-----------------------------------------------------");
+    }
+}
+
