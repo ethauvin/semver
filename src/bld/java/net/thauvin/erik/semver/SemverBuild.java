0d6c051e26fac631b09421d39bcb82deaf9065b6
Erik C. Thauvin <erik@thauvin.net>
2025-07-11 10:48:38 -0700

    Bump JUnit to version 5.13.3

--- src/bld/java/net/thauvin/erik/semver/SemverBuild.java
+++ src/bld/java/net/thauvin/erik/semver/SemverBuild.java
@@ -75,8 +75,8 @@ public class SemverBuild extends Project {
         scope(compile)
                 .include(dependency("com.github.spullara.mustache.java", "compiler", version(0, 9, 14)));
         scope(test)
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 2)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 2)));
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 3)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 3)));
 
 
         javadocOperation().javadocOptions()
