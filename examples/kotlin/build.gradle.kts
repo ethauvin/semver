43235b14ca83415978c1b950d1d2164ab1faf0b2
Erik C. Thauvin <erik@thauvin.net>
2025-08-15 22:44:15 -0700

    Bump Kotlin to version 2.2.10

--- examples/kotlin/build.gradle.kts
+++ examples/kotlin/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     id("application")
     id("com.github.ben-manes.versions") version "0.51.0"
-    kotlin("jvm") version "2.2.0"
+    kotlin("jvm") version "2.2.10"
     kotlin("kapt") version "1.9.25"
 }
 
